368a1ae249fdae96bb3bfd3ad612510dbff9f1c7
Tham94 <tham_the_indian@msn.com>
2022-08-15 16:24:26 +0200

    Starting App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './styles/style.css';
+
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Form from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
