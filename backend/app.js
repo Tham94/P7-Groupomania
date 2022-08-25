b6ae82fa55cf5def4894db2b2a8531af663b6957
Tham94 <tham_the_indian@msn.com>
2022-08-25 09:18:49 +0200

    backend: cors config updated

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,7 @@ const postRoutes = require('./routes/post');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'server is running' });
