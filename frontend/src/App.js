063881aac48db4ce5bd1eb09b3e1dad1d3972dbd
Tham94 <tham_the_indian@msn.com>
2022-08-31 16:35:55 +0200

    context provided in App.js to reach authenticated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,36 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Forum from './pages/Forum';
 import Contact from './pages/Contact';
+import Profile from './pages/Profile';
+import DisplayPost from './components/DisplayPost';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import { hasAuthenticated } from './services/AuthApi';
+import Auth from './contexts/Auth';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
+
   return (
-    <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/forum" element={<Forum />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
-    </div>
+    <Auth.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <div className="App">
+        <Layout>
+          <Routes>
+            <Route path="*" element={<Navigate to={'/login'} />} />
+            <Route exact path="/login" element={<Home />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route element={<AuthenticatedRoute />}>
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/forum" element={<Forum />} />
+              <Route exact path="/forum/:id" element={<DisplayPost />} />
+            </Route>
+          </Routes>
+        </Layout>
+      </div>
+    </Auth.Provider>
   );
 }
 
