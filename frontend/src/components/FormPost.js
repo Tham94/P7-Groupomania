5cf110ce703db1484788e419980f20ac65a78f20
Tham94 <tham_the_indian@msn.com>
2022-09-07 16:21:38 +0200

    using formik for sending post with redirecing to forum page after posting

--- frontend/src/components/FormPost.js
+++ frontend/src/components/FormPost.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import AxiosClient from '../client/AxiosClient';
 import { getToken } from '../services/LocalStorage';
 
@@ -23,8 +25,10 @@ function FormPost() {
         method: 'post',
         url: 'api/posts/',
         headers: { Authorization: 'Bearer ' + userToken },
+        'Content-Type': 'multipart/form-data',
         data: formData,
       });
+      window.location = '/forum';
     } catch (error) {
       console.log(error);
     }
@@ -34,39 +38,62 @@ function FormPost() {
     <>
       <section className="Forum__post-container">
         <h2 className="Forum__post-h2">Postes un message !</h2>
-        <form type="POST" className="Forum__post" onSubmit={handlePost}>
-          <input
-            type="text"
-            className="Forum__post-title"
-            required
-            placeholder="Titre *"
-            name="title"
-            aria-label="titre du post"
-          ></input>
+        <Formik
+          initialValues={{ title: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.title) {
+              errors.title = 'Vous devez renseigner un titre';
+            }
+            return errors;
+          }}
+          onSubmit={handlePost}
+        >
+          <Form
+            className="Forum__post"
+            encType="multipart/form-data"
+            method="post"
+          >
+            <Field
+              type="text"
+              className="Forum__post-title"
+              required
+              placeholder="Titre *"
+              name="title"
+              aria-label="titre du post"
+              autoFocus={true}
+            />
+            <ErrorMessage
+              name="title"
+              component="span"
+              className="Form__alert"
+            />
 
-          <textarea
-            type="text"
-            className="Forum__post-content"
-            placeholder="Ton message"
-            name="content"
-            aria-label="contenu du post"
-          ></textarea>
+            <Field
+              type="text"
+              as="textarea"
+              className="Forum__post-content"
+              placeholder="Ton message"
+              name="content"
+              aria-label="contenu du post"
+            />
 
-          <input
-            type="file"
-            name="image"
-            id="img-upload"
-            className="Forum__post-img"
-            accept=".png, .jpg, .jpeg"
-            aria-label="ajouter image"
-            onChange={(e) => {
-              setImage(e.target.files[0]);
-            }}
-          ></input>
-          <button type="submit" className="Forum__post-submit">
-            Je postes !
-          </button>
-        </form>
+            <Field
+              type="file"
+              name="image"
+              id="img-upload"
+              className="Forum__post-img"
+              accept=".png, .jpg, .jpeg"
+              aria-label="ajouter image"
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+              }}
+            />
+            <button type="submit" className="Forum__post-submit">
+              Je postes !
+            </button>
+          </Form>
+        </Formik>
       </section>
     </>
   );
