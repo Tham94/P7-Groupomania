260d727372ded78c104156a019b0492b5ad80013
Tham94 <tham_the_indian@msn.com>
2022-09-18 01:34:53 +0200

    Formik removed because can't handle changes properly

--- frontend/src/components/FormPost.js
+++ frontend/src/components/FormPost.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import AxiosClient from '../client/AxiosClient';
 import { getToken } from '../services/LocalStorage';
 
 /**
- * [FormPost description]
+ * [Envoi d'un message contenant ou non une image, titre requis
+ *  Une fois post envoyé, rafraichissement de la page ]
  *
  * @return  {JSX.Element}  [Composant permettant de poster un message]
  */
 function FormPost() {
+  const [isSubmited, setIsSubmited] = useState(false);
   const [image, setImage] = useState(undefined);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-  const handlePost = async (token) => {
-    const title = document.querySelector('.Forum__post-title').value;
-    const content = document.querySelector('.Forum__post-content').value;
+  useEffect(() => {
+    if (isSubmited) {
+      window.location.reload();
+    }
+  }, [isSubmited]);
+
+  /**
+   * [ Soumission du formulaire :
+   *   construction d'un nouvel objet FormData : avec données récupérer en dynamiques]
+   *
+   * @param   {event}  e      [évènement de soumission du formulaire]
+   * @param   {string}  token  [token de l'utilisateur connecté]
+   *
+   *
+   */
+  const handlePost = async (e, token) => {
+    e.preventDefault();
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
@@ -28,7 +45,7 @@ function FormPost() {
         'Content-Type': 'multipart/form-data',
         data: formData,
       });
-      window.location = '/forum';
+      setIsSubmited(true);
     } catch (error) {
       console.log(error);
     }
@@ -38,62 +55,51 @@ function FormPost() {
     <>
       <section className="Forum__post-container">
         <h2 className="Forum__post-h2">Postes un message !</h2>
-        <Formik
-          initialValues={{ title: '' }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.title) {
-              errors.title = 'Vous devez renseigner un titre';
-            }
-            return errors;
-          }}
+
+        <form
+          encType="multipart/form-data"
+          className="Forum__post"
           onSubmit={handlePost}
         >
-          <Form
-            className="Forum__post"
-            encType="multipart/form-data"
-            method="post"
-          >
-            <Field
-              type="text"
-              className="Forum__post-title"
-              required
-              placeholder="Titre *"
-              name="title"
-              aria-label="titre du post"
-              autoFocus={true}
-            />
-            <ErrorMessage
-              name="title"
-              component="span"
-              className="Form__alert"
-            />
+          <input
+            type="text"
+            className="Forum__post-title"
+            required
+            placeholder="Titre *"
+            name="title"
+            aria-label="titre du post"
+            autoFocus={true}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
 
-            <Field
-              type="text"
-              as="textarea"
-              className="Forum__post-content"
-              placeholder="Ton message"
-              name="content"
-              aria-label="contenu du post"
-            />
+          <textarea
+            type="text"
+            className="Forum__post-content"
+            placeholder="Ton message"
+            name="content"
+            aria-label="contenu du post"
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
 
-            <Field
-              type="file"
-              name="image"
-              id="img-upload"
-              className="Forum__post-img"
-              accept=".png, .jpg, .jpeg"
-              aria-label="ajouter image"
-              onChange={(e) => {
-                setImage(e.target.files[0]);
-              }}
-            />
-            <button type="submit" className="Forum__post-submit">
-              Je postes !
-            </button>
-          </Form>
-        </Formik>
+          <input
+            type="file"
+            name="image"
+            id="img-upload"
+            className="Forum__post-img"
+            accept=".png, .jpg, .jpeg"
+            aria-label="ajouter image"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          />
+          <button type="submit" className="Forum__post-submit">
+            Je postes !
+          </button>
+        </form>
       </section>
     </>
   );
