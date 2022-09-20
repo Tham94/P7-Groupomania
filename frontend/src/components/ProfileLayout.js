579e696cc600c9d06702c4ad065a232ca6b40c6a
Tham94 <tham_the_indian@msn.com>
2022-09-20 19:57:53 +0200

    modify name and lastname functions added

--- frontend/src/components/ProfileLayout.js
+++ frontend/src/components/ProfileLayout.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import AxiosClient from '../client/AxiosClient';
 import Auth from '../contexts/Auth';
@@ -6,7 +6,13 @@ import { logOut } from '../services/AuthApi';
 import { getToken } from '../services/LocalStorage';
 
 function ProfileLayout() {
-  const { user } = useContext(Auth);
+  const { user, setUser } = useContext(Auth);
+
+  const [isUpdatingName, setIsUpdatingName] = useState(false);
+  const [isUpdatingLastName, setIsUpdatingLastName] = useState(false);
+  const [image, setImage] = useState(undefined);
+  const [nameUpdated, setNameUpdated] = useState(user.name);
+  const [lastNameUpdated, setLastNameUpdated] = useState(user.lastName);
 
   const token = getToken('sessionToken');
 
@@ -17,12 +23,52 @@ function ProfileLayout() {
       return false;
     }
   };
+  const modifyImage = async () => {
+    const file = new FormData();
+    file.append('image', image);
+    try {
+      await AxiosClient({
+        method: 'put',
+        url: `api/auth/users/${user.id}/image`,
+        headers: { Authorization: 'Bearer ' + token },
+        'Content-Type': 'multipart/form-data',
+        data: { file },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  /**
-   * [Suppression d'un compte utilisateur]
-   *
-   *
-   */
+  const modifyName = async () => {
+    try {
+      await AxiosClient({
+        method: 'put',
+        url: `api/auth/users/${user.id}/name`,
+        headers: { Authorization: 'Bearer ' + token },
+        data: { name: nameUpdated },
+      });
+      setUser({ ...user, name: nameUpdated });
+      setIsUpdatingName(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const modifyLastName = async () => {
+    try {
+      await AxiosClient({
+        method: 'put',
+        url: `api/auth/users/${user.id}/lastname`,
+        headers: { Authorization: 'Bearer ' + token },
+        data: { lastName: lastNameUpdated },
+      });
+      setUser({ ...user, lastName: lastNameUpdated });
+      setIsUpdatingLastName(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const deleteUser = async () => {
     const confirmation = window.confirm(
       'ATTENTION :Voulez-vous définitivement vous désinscrire?'
@@ -63,29 +109,105 @@ function ProfileLayout() {
           alt="photo_de_profil"
           src={user.imageUrl}
         ></img>
+        <div>
+          <input
+            type="file"
+            name="image"
+            id="img-upload"
+            className="Profile__pic-selector"
+            accept=".png, .jpg, .jpeg"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          />
+          <button
+            onClick={() => {
+              modifyImage();
+            }}
+          >
+            <i className="fa-solid fa-user-check Modifying__post-icon"></i>
+          </button>
+        </div>
       </div>
+
       <div className="Profile__user">
         <div className="Profile__user-details">
           <div className="User__details-name">
             <div>
               Prénom : <span className="User__name">{user.name}</span>
             </div>
-            {!isAdmin() && (
-              <div>
+            {!isAdmin() && !isUpdatingName && (
+              <div
+                onClick={() => {
+                  setIsUpdatingName(true);
+                }}
+              >
                 <i className="fa-solid fa-pencil Modifying__post-icon"></i>
               </div>
             )}
+            {isUpdatingName && (
+              <>
+                <input
+                  className="Profile__edit-name"
+                  type="text"
+                  placeholder="Prénom"
+                  value={nameUpdated}
+                  aria-label="prénom utilisateur"
+                  onChange={(e) => setNameUpdated(e.target.value)}
+                ></input>
+                <button type="submit" onClick={modifyName}>
+                  Valider
+                </button>
+                <div
+                  className="Modifying-name-cancel"
+                  onClick={() => {
+                    setIsUpdatingName(false);
+                  }}
+                >
+                  <i className="fa-solid fa-backward"></i>
+                </div>
+              </>
+            )}
           </div>
         </div>
+
         <div className="Profile__user-details">
           <div className="User__details-name">
             <div>
               Nom : <span className="User__last-name">{user.lastName}</span>
             </div>
-            {!isAdmin() && (
-              <div>
+            {!isAdmin() && !isUpdatingLastName && (
+              <div
+                onClick={() => {
+                  setIsUpdatingLastName(true);
+                }}
+              >
                 <i className="fa-solid fa-pencil Modifying__post-icon"></i>
               </div>
+            )}
+
+            {isUpdatingLastName && (
+              <>
+                <input
+                  className="Profile__edit-name"
+                  type="text"
+                  placeholder="Nom"
+                  value={lastNameUpdated}
+                  aria-label="nom utilisateur"
+                  onChange={(e) => setLastNameUpdated(e.target.value)}
+                ></input>
+                <button type="submit" onClick={modifyLastName}>
+                  Valider
+                </button>
+                <div
+                  className="Modifying-name-cancel"
+                  onClick={() => {
+                    setIsUpdatingLastName(false);
+                  }}
+                >
+                  <i className="fa-solid fa-backward"></i>
+                </div>
+              </>
             )}
           </div>
         </div>
